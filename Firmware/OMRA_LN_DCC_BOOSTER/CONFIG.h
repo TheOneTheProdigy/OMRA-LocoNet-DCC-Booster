369951de36581537e904c68496da49f5a3ba29b1
TheOneTheProdigy <141891915+TheOneTheProdigy@users.noreply.github.com>
2026-01-04 01:23:39 -0600

    V2.1.10
    Firmware Changes To Enable New DRV8874 OMRA Modules And Greatly Improved Current Sensing.

--- Firmware/OMRA_LN_DCC_BOOSTER/CONFIG.h
+++ Firmware/OMRA_LN_DCC_BOOSTER/CONFIG.h
@@ -1,7 +1,7 @@
-//  DCC / LocoNet RailSync Booster - Dual 5A Booster Districts With Overload Protection, Overload Alarm Output, And OLED Screen With Both Booster's Current and Status
+//  DCC / LocoNet RailSync Booster - Dual 6A Booster Districts With Overload Protection, Overload Alarm Output, And OLED Screen With Both Booster's Current and Status
 
 //  2024-2026 Lance Bradley  Ozarks Model Railroad Assocation
-//  2024 Kurt Clement   Ozarks Model Railroad Assocation
+//  2024-2026 Kurt Clement   Ozarks Model Railroad Assocation
 
 //  This Is The Firmware For The 10A-X2 Booster For The OMRA Club In Springfield Missouri
 
@@ -13,11 +13,22 @@ float DEBUG_REFRESH_TIME = 2500; // Milliseconds to wait before posting another 
 float BOOST1_CS_OFFSET = 0.2174; // Zeros the offset for current measurements. This is a voltage on the current sense pin.  
 float BOOST2_CS_OFFSET = 0.2174; // + Decreases - Increases current readings. If VREF 5.0V / 0.2174V = 0A.
 
-// Overcurrent Protection
-float OC_LIMIT = 4; // Limit to trigger the over current protection in amps. Anything above this will blow after the inrush delay.
-float OC_TIMEOUT = 2000; // Milliseconds to wait to energize the track again between short circuits.
+// Instant Overcurrent Protection
+float OC_LIMIT1 = 4.0; // Limit to trigger the over current protection in amps. Anything above this will blow after the inrush delay.
+float OC_LIMIT2 = 4.0; // Limit to trigger the over current protection in amps. Anything above this will blow after the inrush delay.
+float OC_TIMEOUT1 = 2000; // Milliseconds to wait to energize the track again between short circuits.
+float OC_TIMEOUT2 = 2000; // Milliseconds to wait to energize the track again between short circuits.
 unsigned long OC_BOOST1_INRUSH = 150; // Milliseconds to allow a inrush of current before tripping instant overcurrent protection. Useful for allowing large keepalives to be powered on and not detect as a short.
 unsigned long OC_BOOST2_INRUSH = 150; // Milliseconds to allow a inrush of current before tripping instant overcurrent protection. Useful for allowing large keepalives to be powered on and not detect as a short.
+
+// Thermal Overcurrent Protection
+bool ENABLE_THERMAL_PROTECTION = true; // Enable or disable the thermal overcurrent protection.
+float OC_THERM_LIMIT1 = 3.25; // Limit to trigger the thermal over current protection in amps. Anything above this will blow after the OC_BOOST_THERM delay.
+float OC_THERM_LIMIT2 = 3.25; // Limit to trigger the thermal over current protection in amps. Anything above this will blow after the OC_BOOST_THERM delay.
+float OC_THERM_TIMEOUT1 = 4000; // Milliseconds to wait to energize the track again between thermal overloads.
+float OC_THERM_TIMEOUT2 = 4000; // Milliseconds to wait to energize the track again between thermal overloads.
+unsigned long OC_BOOST1_THERM = 8000; // Milliseconds to allow a inrush of current before tripping thermal overcurrent protection. Useful for allowing large keepalives to be powered on and not detect as a short.
+unsigned long OC_BOOST2_THERM = 8000; // Milliseconds to allow a inrush of current before tripping thermal overcurrent protection. Useful for allowing large keepalives to be powered on and not detect as a short.
 
 // Railsync Monitoring / Booster Enable
 float PWM_TIMER_LIMIT = 250; // Milliseconds to go without valid railsync commands before boosters shutdown. 
@@ -25,14 +36,3 @@ int PWM_SIG_EDGES = 2; // Edges to trigger railsync active or not within PWM_TRI
 
 // OLED HMI Display
 float PRINT_DISPLAY_DELAY_TIME = 100; // Refresh OLED screen 10 times a second.
-
-// Micro Pin Assignments
-int C_SENSE1_MICRO = A1; // Booster1 current sensing pin.
-int C_SENSE2_MICRO = A2; // Booster2 current sensing pin.
-int EN1_MICRO = 21; // Booster1 enable pin.
-int EN2_MICRO = 18; // Booster2 enable pin
-int PWM_DETECT_MICRO = 4; // Pin to detect valid railsync packets and shutdown track power if no signal.
-int LN_RX_MICRO = 8; // Pin To Receive LocoNet Packets From The LocoNet Buss
-int LN_TX_MICRO = 9; // Pin To Send LocoNet Packets To The LocoNet Buss
-int ALM1_MICRO = 10; // Pin To Activate LED and Piezo Alarm For Booster 1 Short Circuit Trip
-int ALM2_MICRO = 5;  // Pin To Activate LED and Piezo Alarm For Booster 2 Short Circuit Trip
--- Firmware/OMRA_LN_DCC_BOOSTER/OMRA_LN_DCC_BOOSTER.ino
+++ Firmware/OMRA_LN_DCC_BOOSTER/OMRA_LN_DCC_BOOSTER.ino
@@ -5,6 +5,19 @@
 
 //  This Is The Firmware For The 10A-X2 Booster For The OMRA Club In Springfield Missouri
 
+// Micro Pin Assignments
+int C_SENSE1_MICRO = A1; // Booster1 current sensing pin.
+int C_SENSE2_MICRO = A2; // Booster2 current sensing pin.
+int N_FAULT1_MICRO = 14; // Booster 1 instant DRV8874 fault pin.
+int N_FAULT2_MICRO = 15; // Booster 2 instant DRV8874 fault pin.
+int EN1_MICRO = 21; // Booster1 enable pin.
+int EN2_MICRO = 18; // Booster2 enable pin
+int PWM_DETECT_MICRO = 4; // Pin to detect valid railsync packets and shutdown track power if no signal.
+int LN_RX_MICRO = 8; // Pin To Receive LocoNet Packets From The LocoNet Buss
+int LN_TX_MICRO = 9; // Pin To Send LocoNet Packets To The LocoNet Buss
+int ALM1_MICRO = 10; // Pin To Activate LED and Piezo Alarm For Booster 1 Short Circuit Trip
+int ALM2_MICRO = 5;  // Pin To Activate LED and Piezo Alarm For Booster 2 Short Circuit Trip
+
 // *** Below Should Not Need Touched ***
 #include <SPI.h>
 #include <Wire.h>
@@ -27,15 +40,27 @@ bool PWM_TIMER_ACTIVE = false;
 bool BOOST1_ENABLED = false;
 bool IS_POWER1_TRIPPED = false;
 bool IS_POWER1_INSTANT_PRE_TRIPPED = false;
+bool IS_POWER1_THERMAL_PRE_TRIPPED = false;
+bool IS_POWER1_DRV8874_OK = false;
+bool IS_POWER1_DRV8874_TRIPPED = false;
+bool IS_POWER1_THERMAL_TRIPPED = false;
 bool BOOST2_ENABLED = false;
 bool IS_POWER2_TRIPPED = false;
 bool IS_POWER2_INSTANT_PRE_TRIPPED = false;
+bool IS_POWER2_THERMAL_PRE_TRIPPED = false;
+bool IS_POWER2_DRV8874_OK = false;
+bool IS_POWER2_DRV8874_TRIPPED = false;
+bool IS_POWER2_THERMAL_TRIPPED = false;
 float BOOST1_AMPS = 0;
 float BOOST2_AMPS = 0;
 float LAST_PRINT_DISPLAY_TIME = 0;
 float LAST_DEBUG_REFRESH_TIME = 0;
 unsigned long POWER1_INSTANT_PRE_TIME = 0;
+unsigned long POWER1_THERMAL_PRE_TIME = 0;
+unsigned long POWER1_DRV8874_NFAULT_TIME = 0;
 unsigned long POWER2_INSTANT_PRE_TIME = 0;
+unsigned long POWER2_THERMAL_PRE_TIME = 0;
+unsigned long POWER2_DRV8874_NFAULT_TIME = 0;
 unsigned long BOOSTER1_SHUTDOWN_TIME = 0;
 unsigned long BOOSTER2_SHUTDOWN_TIME = 0;
 unsigned long CURRENT_CYCLE_TIME = 0;
@@ -45,7 +70,7 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 // Start Setup
 void setup() {
 
-  if (PRINT_DEBUG == true){
+  if (PRINT_DEBUG){
     Serial.begin(9600);
   }
 
@@ -55,6 +80,8 @@ void setup() {
   pinMode(LN_TX_MICRO, OUTPUT);
   pinMode(ALM1_MICRO, OUTPUT);
   pinMode(ALM2_MICRO, OUTPUT);
+  pinMode(N_FAULT1_MICRO, INPUT_PULLUP);
+  pinMode(N_FAULT2_MICRO, INPUT_PULLUP);
 
   
   // Start Display
@@ -97,7 +124,7 @@ void setup() {
   display.setTextColor(WHITE);
   display.setCursor(22, 0);
   display.setTextWrap(false);
-  display.println("V2.1.07");
+  display.println("V2.1.10");
   display.drawBitmap(0, 16, OMRALOGO, 128, 64, WHITE);
   display.display();
   delay(1500);
@@ -113,11 +140,13 @@ void turnPower1On() {
   digitalWrite(EN1_MICRO, HIGH);
   BOOST1_ENABLED = true;
   IS_POWER1_TRIPPED = false;
+  IS_POWER1_THERMAL_TRIPPED = false;
 }
 void turnPower2On() {
   digitalWrite(EN2_MICRO, HIGH);
   BOOST2_ENABLED = true;
   IS_POWER2_TRIPPED = false;
+  IS_POWER2_THERMAL_TRIPPED = false;
 }
 void turnPower1Off() {
   digitalWrite(EN1_MICRO, LOW);
@@ -142,19 +171,54 @@ void loop() {
   BOOST1_AMPS = ((analogReadFast(C_SENSE1_MICRO) * 0.00488) - BOOST1_CS_OFFSET) / 0.4352; // Each ADC step represents approx 4.88mV. .4352 = .000455 x 956.5 where .000455 is uA/A and 1K + 22K = 956.5.
   BOOST1_AMPS = constrain(BOOST1_AMPS, 0, 9.9); // Filter Below 0A and above 9.9A..
 
-  // Over current fault trip routine  
-  if (BOOST1_AMPS >= OC_LIMIT && IS_POWER1_TRIPPED == false && IS_POWER1_INSTANT_PRE_TRIPPED == false) {
+  // Over current fault trip routine
+  if (BOOST1_AMPS >= OC_LIMIT1 && !IS_POWER1_TRIPPED && !IS_POWER1_INSTANT_PRE_TRIPPED) {
       POWER1_INSTANT_PRE_TIME = CURRENT_CYCLE_TIME;
       IS_POWER1_INSTANT_PRE_TRIPPED = true;
     }
-    else if (IS_POWER1_INSTANT_PRE_TRIPPED && BOOST1_AMPS < OC_LIMIT && (CURRENT_CYCLE_TIME - POWER1_INSTANT_PRE_TIME) >= OC_BOOST1_INRUSH) {
+    else if (IS_POWER1_INSTANT_PRE_TRIPPED && BOOST1_AMPS < OC_LIMIT1 && (CURRENT_CYCLE_TIME - POWER1_INSTANT_PRE_TIME) >= OC_BOOST1_INRUSH) {
       IS_POWER1_INSTANT_PRE_TRIPPED = false;
     }
 
-  if (IS_POWER1_INSTANT_PRE_TRIPPED && IS_POWER1_TRIPPED == false && (CURRENT_CYCLE_TIME - POWER1_INSTANT_PRE_TIME) >= OC_BOOST1_INRUSH) {
+  if (IS_POWER1_INSTANT_PRE_TRIPPED && !IS_POWER1_TRIPPED && (CURRENT_CYCLE_TIME - POWER1_INSTANT_PRE_TIME) >= OC_BOOST1_INRUSH) {
     IS_POWER1_TRIPPED = true;
     BOOSTER1_SHUTDOWN_TIME = CURRENT_CYCLE_TIME;
     turnPower1Off();
+  }
+
+  // DRV8874 NFAULT over current fault routine
+  IS_POWER1_DRV8874_OK = digitalRead(N_FAULT1_MICRO);
+
+  if (!IS_POWER1_DRV8874_OK && !IS_POWER1_DRV8874_TRIPPED) {
+    POWER1_DRV8874_NFAULT_TIME = CURRENT_CYCLE_TIME;
+    IS_POWER1_DRV8874_TRIPPED = true;
+  }
+    else if(IS_POWER1_DRV8874_OK && IS_POWER1_DRV8874_TRIPPED && (CURRENT_CYCLE_TIME - POWER1_DRV8874_NFAULT_TIME) >= OC_BOOST1_INRUSH) {
+      IS_POWER1_DRV8874_TRIPPED = false;
+    } 
+
+  if (!IS_POWER1_DRV8874_OK && !IS_POWER1_TRIPPED && (CURRENT_CYCLE_TIME - POWER1_DRV8874_NFAULT_TIME) >= OC_BOOST1_INRUSH) {
+    IS_POWER1_TRIPPED = true;
+    BOOSTER1_SHUTDOWN_TIME = CURRENT_CYCLE_TIME;
+    turnPower1Off();
+  }
+
+    // Thermal over current fault trip routine
+  if (ENABLE_THERMAL_PROTECTION) {
+    if (BOOST1_AMPS >= OC_THERM_LIMIT1 && !IS_POWER1_TRIPPED && !IS_POWER1_THERMAL_PRE_TRIPPED) {
+        POWER1_THERMAL_PRE_TIME = CURRENT_CYCLE_TIME;
+        IS_POWER1_THERMAL_PRE_TRIPPED = true;
+      }
+      else if (IS_POWER1_THERMAL_PRE_TRIPPED && BOOST1_AMPS < OC_THERM_LIMIT1 && (CURRENT_CYCLE_TIME - POWER1_THERMAL_PRE_TIME) >= OC_BOOST1_INRUSH) {
+        IS_POWER1_THERMAL_PRE_TRIPPED = false;
+      }
+
+    if (IS_POWER1_THERMAL_PRE_TRIPPED && !IS_POWER1_TRIPPED && (CURRENT_CYCLE_TIME - POWER1_THERMAL_PRE_TIME) >= OC_BOOST1_THERM) {
+      IS_POWER1_TRIPPED = true;
+      IS_POWER1_THERMAL_TRIPPED = true;
+      BOOSTER1_SHUTDOWN_TIME = CURRENT_CYCLE_TIME;
+      turnPower1Off();
+    }
   }
 
   // Booster 2
@@ -164,65 +228,105 @@ void loop() {
   BOOST2_AMPS = constrain(BOOST2_AMPS, 0, 9.99); // Filter Below 0A and above 9.9A.
 
   // Over current fault trip routine  
-  if (BOOST2_AMPS >= OC_LIMIT && IS_POWER2_TRIPPED == false && IS_POWER2_INSTANT_PRE_TRIPPED == false) {
+  if (BOOST2_AMPS >= OC_LIMIT2 && !IS_POWER2_TRIPPED && !IS_POWER2_INSTANT_PRE_TRIPPED) {
       POWER2_INSTANT_PRE_TIME = CURRENT_CYCLE_TIME;
       IS_POWER2_INSTANT_PRE_TRIPPED = true;
     }
-    else if (IS_POWER2_INSTANT_PRE_TRIPPED && BOOST2_AMPS < OC_LIMIT && (CURRENT_CYCLE_TIME - POWER2_INSTANT_PRE_TIME) >= OC_BOOST2_INRUSH) {
+    else if (IS_POWER2_INSTANT_PRE_TRIPPED && BOOST2_AMPS < OC_LIMIT2 && (CURRENT_CYCLE_TIME - POWER2_INSTANT_PRE_TIME) >= OC_BOOST2_INRUSH) {
       IS_POWER2_INSTANT_PRE_TRIPPED = false;
     }
 
-  if (IS_POWER2_INSTANT_PRE_TRIPPED && IS_POWER2_TRIPPED == false && (CURRENT_CYCLE_TIME - POWER2_INSTANT_PRE_TIME) >= OC_BOOST2_INRUSH) {
+  if (IS_POWER2_INSTANT_PRE_TRIPPED && !IS_POWER2_TRIPPED && (CURRENT_CYCLE_TIME - POWER2_INSTANT_PRE_TIME) >= OC_BOOST2_INRUSH) {
     IS_POWER2_TRIPPED = true;
     BOOSTER2_SHUTDOWN_TIME = CURRENT_CYCLE_TIME;
     turnPower2Off();
   }
 
+    // DRV8874 2 NFAULT over current fault routine
+  IS_POWER2_DRV8874_OK = digitalRead(N_FAULT2_MICRO);
+
+  if (!IS_POWER2_DRV8874_OK && !IS_POWER2_DRV8874_TRIPPED) {
+    POWER2_DRV8874_NFAULT_TIME = CURRENT_CYCLE_TIME;
+    IS_POWER2_DRV8874_TRIPPED = true;
+  }
+    else if(IS_POWER2_DRV8874_OK && IS_POWER2_DRV8874_TRIPPED && (CURRENT_CYCLE_TIME - POWER2_DRV8874_NFAULT_TIME) >= OC_BOOST2_INRUSH) {
+      IS_POWER1_DRV8874_TRIPPED = false;
+    } 
+
+  if (!IS_POWER2_DRV8874_OK && !IS_POWER2_TRIPPED && (CURRENT_CYCLE_TIME - POWER2_DRV8874_NFAULT_TIME) >= OC_BOOST2_INRUSH) {
+    IS_POWER2_TRIPPED = true;
+    BOOSTER2_SHUTDOWN_TIME = CURRENT_CYCLE_TIME;
+    turnPower2Off();
+  }
+
+    // Thermal over current fault trip routine
+  if (ENABLE_THERMAL_PROTECTION) {
+    if (BOOST2_AMPS >= OC_THERM_LIMIT2 && !IS_POWER2_TRIPPED && !IS_POWER2_THERMAL_PRE_TRIPPED) {
+        POWER2_THERMAL_PRE_TIME = CURRENT_CYCLE_TIME;
+        IS_POWER2_THERMAL_PRE_TRIPPED = true;
+      }
+      else if (IS_POWER2_THERMAL_PRE_TRIPPED && BOOST2_AMPS < OC_THERM_LIMIT2 && (CURRENT_CYCLE_TIME - POWER2_THERMAL_PRE_TIME) >= OC_BOOST2_INRUSH) {
+        IS_POWER2_THERMAL_PRE_TRIPPED = false;
+      }
+
+    if (IS_POWER2_THERMAL_PRE_TRIPPED && !IS_POWER2_TRIPPED && (CURRENT_CYCLE_TIME - POWER2_THERMAL_PRE_TIME) >= OC_BOOST2_THERM) {
+      IS_POWER2_TRIPPED = true;
+      IS_POWER2_THERMAL_TRIPPED = true;
+      BOOSTER2_SHUTDOWN_TIME = CURRENT_CYCLE_TIME;
+      turnPower2Off();
+    }
+  }
+
   // Attempt To Repower The Boosters
 
   // Booster 1 Repower Procedure
-  if ((IS_POWER1_TRIPPED == true) && ((CURRENT_CYCLE_TIME - BOOSTER1_SHUTDOWN_TIME) >= OC_TIMEOUT)) { // Retry if timer resets.
+  if ((IS_POWER1_THERMAL_TRIPPED) && (IS_POWER1_TRIPPED) && ((CURRENT_CYCLE_TIME - BOOSTER1_SHUTDOWN_TIME) >= OC_THERM_TIMEOUT1)) {
+    turnPower1On();
+  }
+    else if ((!IS_POWER1_THERMAL_TRIPPED) && (IS_POWER1_TRIPPED) && ((CURRENT_CYCLE_TIME - BOOSTER1_SHUTDOWN_TIME) >= OC_TIMEOUT1)) { // Retry if timer resets.
       turnPower1On();
-  }
-
+    }
   // Booster 2 Repower Procedure
-  if ((IS_POWER2_TRIPPED == true) && ((CURRENT_CYCLE_TIME - BOOSTER2_SHUTDOWN_TIME) >= OC_TIMEOUT)) { // Retry if timer resets.
-      turnPower2On();
+  if ((IS_POWER2_THERMAL_TRIPPED) && (IS_POWER2_TRIPPED) && ((CURRENT_CYCLE_TIME - BOOSTER2_SHUTDOWN_TIME) >= OC_THERM_TIMEOUT2)) {
+    turnPower2On();
   }
+    else if ((!IS_POWER2_THERMAL_TRIPPED) && (IS_POWER2_TRIPPED) && ((CURRENT_CYCLE_TIME - BOOSTER2_SHUTDOWN_TIME) >= OC_TIMEOUT2)) { // Retry if timer resets.
+      turnPower2On();
+    }
 
   // Draw Display
   if (millis() - LAST_PRINT_DISPLAY_TIME >= PRINT_DISPLAY_DELAY_TIME) {
     display.clearDisplay(); // Clear Previous Display
 
     // Display Icons In Yellow Bar    
-    if (BOOST1_ENABLED == true) {     // Booster 1 Enabled Icon
+    if (BOOST1_ENABLED) {     // Booster 1 Enabled Icon
       display.drawBitmap(0, 0, POWERICON, 16, 16, WHITE);
       digitalWrite(ALM1_MICRO, LOW);  // Deactivate The Short Circuit Alarm   
     }
-    if (BOOST2_ENABLED == true) {     // Booster 2 Enabled Icon
+    if (BOOST2_ENABLED) {     // Booster 2 Enabled Icon
       display.drawBitmap(64, 0, POWERICON, 16, 16, WHITE);   
       digitalWrite(ALM2_MICRO, LOW);  // Deactivate The Short Circuit Alarm   
     }
-    if (IS_POWER1_TRIPPED == true) {     // Booster 1 Overload Icon
+    if (IS_POWER1_TRIPPED) {     // Booster 1 Overload Icon
       display.drawBitmap(16, 0, OVERLOADICON, 16, 16, WHITE); 
       digitalWrite(ALM1_MICRO, HIGH); // Activate The Short Circuit alarm 
     }
-    if (IS_POWER2_TRIPPED == true) {     // Booster 2 Overload Icon
+    if (IS_POWER2_TRIPPED) {     // Booster 2 Overload Icon
       display.drawBitmap(80, 0, OVERLOADICON, 16, 16, WHITE);   
       digitalWrite(ALM2_MICRO, HIGH); // Activate The Short Circuit alarm 
     }
-    if (IS_POWER1_INSTANT_PRE_TRIPPED == true) {     
+    if (IS_POWER1_DRV8874_TRIPPED) {     
       display.drawBitmap(32, 0, ALARM1ICON, 16, 16, WHITE);   
     }
-    if (IS_POWER2_INSTANT_PRE_TRIPPED == true) {     
+    if (IS_POWER2_DRV8874_TRIPPED) {     
       display.drawBitmap(96, 0, ALARM1ICON, 16, 16, WHITE);   
     }
-    // if () {     
-    //   display.drawBitmap(48, 0, ALARM2ICON, 16, 16, WHITE);   
-    // }
-    // if () {     
-    //   display.drawBitmap(112, 0, ALARM2ICON, 16, 16, WHITE);   
-    // }
+    if (IS_POWER1_INSTANT_PRE_TRIPPED || IS_POWER1_THERMAL_PRE_TRIPPED) {     
+      display.drawBitmap(48, 0, ALARM2ICON, 16, 16, WHITE);   
+    }
+    if (IS_POWER2_INSTANT_PRE_TRIPPED || IS_POWER2_THERMAL_PRE_TRIPPED) {     
+      display.drawBitmap(112, 0, ALARM2ICON, 16, 16, WHITE);   
+    }
     
     // Display Booster District Name and INFO Box
     display.setTextSize(1);
@@ -254,7 +358,7 @@ void loop() {
   }
 
   // Check For Railsync Activity And Turn Off Or ON Track Power Depending On Activity After Alloted Time
-  if (PWM_TIMER_ACTIVE == false){
+  if (!PWM_TIMER_ACTIVE){
     PWM_TIMER = millis();
     PWM_COUNT = 0;
     PWM_TIMER_ACTIVE = true;
@@ -266,10 +370,10 @@ void loop() {
     }
   }  
   if ((millis() - PWM_TIMER) > (PWM_TIMER_LIMIT)) {  
-    if ((PWM_COUNT >= PWM_SIG_EDGES) && (IS_POWER1_TRIPPED == false)) {
+    if ((PWM_COUNT >= PWM_SIG_EDGES) && (!IS_POWER1_TRIPPED)) {
       turnPower1On();
     }
-    if ((PWM_COUNT >= PWM_SIG_EDGES) && (IS_POWER2_TRIPPED == false)) {
+    if ((PWM_COUNT >= PWM_SIG_EDGES) && (!IS_POWER2_TRIPPED)) {
       turnPower2On();
     }
     if (PWM_COUNT <= PWM_SIG_EDGES) {
@@ -280,7 +384,7 @@ void loop() {
   PWM_LAST = PWM_DETECT;
 
   // Debug Code
-   if ((PRINT_DEBUG == true) && (CURRENT_CYCLE_TIME - LAST_DEBUG_REFRESH_TIME >= DEBUG_REFRESH_TIME)) {
+   if ((PRINT_DEBUG) && (CURRENT_CYCLE_TIME - LAST_DEBUG_REFRESH_TIME >= DEBUG_REFRESH_TIME)) {
     LAST_DEBUG_REFRESH_TIME = CURRENT_CYCLE_TIME;
     Serial.println();
     Serial.println();
